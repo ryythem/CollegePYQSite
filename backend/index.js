087117b52fbcf30953814ec6e7621fe447777020
RythemGoyal <imrythem16@gmail.com>
2025-03-20 22:56:24 +0530

    Test route added

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const {UserDBconnection} = require("./db/db.js");
+const { UserDBconnection } = require("./db/db.js");
 const authRoute = require("./routes/auth.js");
-const fileRoute = require("./routes/fileRoute.js")
+const fileRoute = require("./routes/fileRoute.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,14 +13,17 @@ app.use(express.json({ limit: "10mb" }));
 
 UserDBconnection();
 
-
 app.use("/auth", authRoute);
-app.use("/files",fileRoute);
+app.use("/files", fileRoute);
 
-app.use((err,req,res,next)=>{
+app.get("hello", (req, res) => {
+  res.send("Hello");
+});
+
+app.use((err, req, res, next) => {
   console.error("Error:", err.message);
   res.status(500).json({ success: false, message: "Internal Server Error" });
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
